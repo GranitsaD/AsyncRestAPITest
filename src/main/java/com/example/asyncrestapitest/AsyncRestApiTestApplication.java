e7a3ed148d75cfe945b3fb6f0b035811420072f6
Граница Дмитрий <granicadmitrij@MacBook-Air-Granica.local>
2023-04-25 17:02:37 +0300

    Initial commit

---
+++ src/main/java/com/example/asyncrestapitest/AsyncRestApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncrestapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncRestApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncRestApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asyncrestapitest/AsyncRestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asyncrestapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncRestApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
